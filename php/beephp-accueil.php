c7dc83edc394189b160b53b57fe307e59d26d4fa
Unknown <laurianepierre@outlook.fr>
2017-01-19 09:16:20 +0100

    html accueil

--- php/beephp-accueil.php
+++ php/beephp-accueil.php
@@ -22,29 +22,41 @@ catch(PDOException $e) {
 
     $liste_article = "SELECT titre_article FROM article";
     $article_recent = "SELECT titre_article, max(date_a) FROM article";
-    $article_plus_lu = "SELECT titre_article, max(nombre_lu)FROM article";
+    $article_plus_lu = "SELECT titre_article, max(nombre_lu) FROM article";
     $article_moins_lu = "SELECT titre_article, min(nombre_lu) FROM article";
 
-function article(){
-    echo $liste_article;
-}
-
 function articlerecent(){
-    $article_image = "SELECT url_img FROM article WHERE $article_recent = titre_article";
-    echo $article_recent;
-    echo $article_image;
-}
+    $article_image = "SELECT url_img FROM article WHERE $article_recent = titre_article";?>
+    <div class="row">
+        <div class="col-md-4 col-sm-12">
+            <?=$article_recent;?>
+            <?=$article_image;?>
+        </div>
+<?php }
 
 function articlepluslu(){
-    $article_image = "SELECT url_img FROM article WHERE $article_recent = titre_article";
-    echo $article_plus_lu;
-    echo $article_image;
-}
+    $article_image = "SELECT url_img FROM article WHERE $article_recent = titre_article";?>
+    <div class="row">
+        <div class="col-md-4 col-sm-12">
+            <?=$article_plus_lu;?>
+            <?=$article_image;?>
+        </div>
+<?php }
 
 function articlemoinslu(){
-    $article_image = "SELECT url_img FROM article WHERE $article_recent = titre_article";
-    echo $article_moins_lu;
-    echo $article_image;
-}
+    $article_image = "SELECT url_img FROM article WHERE $article_recent = titre_article";?>
+    <div class="row">
+        <div class="col-md-4 col-sm-12">
+            <?=$article_moins_lu;?>
+            <?=$article_image;?>
+        </div>
+    </div>
+<?php }
+
+function article(){?>
+    <div class="row">
+        <?=$liste_article;?>
+    </div>
+<?php}
 
 ?>
